dae39ee216b4466407305c44691c0e0461f6035f
Nguyen Duong The Vi <nguyenduongthevi31415@gmail.com>
2025-03-06 15:36:35 +0700

    Update

--- actions/EditorAjax.php
+++ actions/EditorAjax.php
@@ -13,6 +13,9 @@ class Settings_PipelineConfig_EditorAjax_Action extends Vtiger_Action_Controller
 
 	function __construct() {
 		$this->exposeMethod('getRoleList');
+		$this->exposeMethod('getPipelineStageInfo');
+		$this->exposeMethod('checkCondition');
+
 	}
     public function checkPermission(Vtiger_Request $request) {
 		$hasPermission = true;
@@ -44,6 +47,70 @@ class Settings_PipelineConfig_EditorAjax_Action extends Vtiger_Action_Controller
             $response->setResult($result);
         } catch (Exception $e) {
             $response->setError($e->getCode(), $e->getMessage());
+        }
+        $response->emit();
+    }
+	public function getPipelineStageInfo(Vtiger_Request $request) {
+        $response = new Vtiger_Response();
+        try {
+            $recordId = $request->get('record');
+            $moduleName = $request->get('modulename');
+           
+            $pipelineStageInfo = PipelineAction::getPipelineStageInfo($recordId, $moduleName);
+            
+        
+            $response->setResult(array(
+                'success' => true,
+                'data' =>$pipelineStageInfo
+            ));
+        } catch(Exception $e) {
+            $response->setError($e->getMessage());
+        }
+        $response->emit();
+    }
+	public function checkCondition(Vtiger_Request $request) {
+        $response = new Vtiger_Response();
+        try {
+            $recordId = $request->get('recordId');
+            $stageIdNext = $request->get('stageIdNext');
+            $moduleName = $request->get('moduleName');
+
+             //Begin edit by The Vi 3/4/2025
+            $recordModel = Vtiger_Record_Model::getInstanceById($recordId, $moduleName);
+            $stageId = $recordModel->get('stageid');
+           
+            // End edit by The Vi 3/4/2025
+            $result = true;
+            if($stageId != $stageIdNext) {
+                $result = PipelineAction::checkConditions($recordId, $stageId, "Potentials");
+            }
+            if ($result) {
+                $response->setResult([
+                    'status'  => true,
+                    'message' => 'Điều kiện chuyển stage thỏa mãn',
+                    'data'    => [
+                        'recordId' => $recordId,
+                        'stageId' => $stageId,
+                        'result' => $result
+                    ]
+                ]);
+            } else {
+                $response->setResult([
+                    'status'  => false,
+                    'message' => 'Điều kiện chuyển stage không thỏa mãn',
+                    'data'    => [
+                        'recordId' => $recordId, 
+                        'stageId' => $stageId,
+                        'result' => $result
+
+                    ]
+                ]);
+            }
+        } catch (Exception $e) {
+            $response->setResult([
+                'status'  => false,
+                'message' => $e->getMessage()
+            ]);
         }
         $response->emit();
     }

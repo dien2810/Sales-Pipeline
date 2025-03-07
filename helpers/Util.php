e1c2efec5101ba1732afe723896af75f9dfe4faf
Nguyen Duong The Vi <nguyenduongthevi31415@gmail.com>
2025-03-07 14:49:52 +0700

    Fix lỗi đối với module khác cơ hội

--- helpers/Util.php
+++ helpers/Util.php
@@ -1,24 +1,13 @@
 <?php
-/*+***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- *************************************************************************************/
 
-/**
- * Helper methods to work with ShortURLs
- */
 class Settings_PipelineConfig_Util_Helper {
     /**
-     * Trả về ID dựa vào tên module
-     * @param string $moduleName Tên module
-     * @return int|null ID của module hoặc null nếu không tìm thấy
+     * Returns the ID based on the module name
+     * @param string $moduleName Module name
+     * @return int|null Module ID or null if not found
      */
     public static function getModuleIdByName($moduleName) {
-        // Danh sách các module và ID tương ứng
+        // List of modules and corresponding IDs
         $moduleIds = [
             'Leads' => 50,
             'Potentials' => 118,
@@ -26,7 +15,7 @@ class Settings_PipelineConfig_Util_Helper {
             'Project' => 652
         ];
 
-        // Kiểm tra moduleName có trong danh sách không và trả về ID
+        // Check if moduleName exists in the list and return its ID
         return $moduleIds[$moduleName] ?? null;
     }
 }

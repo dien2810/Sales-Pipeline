9cedda81d44e47534ff6bdf95421e4fc01cbc18d
Nguyen Duong The Vi <nguyenduongthevi31415@gmail.com>
2025-03-10 14:35:58 +0700

    Kiểm tra bước chuyển đến cho phép

--- actions/EditorAjax.php
+++ actions/EditorAjax.php
@@ -82,7 +82,7 @@ class Settings_PipelineConfig_EditorAjax_Action extends Vtiger_Action_Controller
             // End edit by The Vi 3/4/2025
             $result = true;
             if($stageId != $stageIdNext) {
-                $result = PipelineAction::checkConditions($recordId, $stageId, $moduleName);
+                $result = PipelineAction::checkConditions($recordId, $stageId, $moduleName, $stageIdNext);
             }
             if ($result) {
                 $response->setResult([
